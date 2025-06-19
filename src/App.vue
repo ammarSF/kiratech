e390bf9763660f29db23bc5948879af62aabf19a
ammar <riku.sf@hotmail.my>
2025-06-19 16:53:12 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <header>
+    <Header />
+
+    <div class="wrapper">
+      
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f5f5f5;
+  padding: 20px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex justify-start">
+    <img alt="KiraTech logo" class="logo" src="@/assets/kiratech-logo.png" />
+  </div>
+</template>
